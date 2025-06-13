3580c141397f747bea6a3aed8c8a894e7c8a2738
vikram katkar <vikramkatkar17@gmail.com>
2025-06-13 17:06:09 +0530

    kafka-remove-debuger

--- src/main/java/com/prevoyance/controller/OrderController.java
+++ src/main/java/com/prevoyance/controller/OrderController.java
@@ -31,7 +31,6 @@ public class OrderController {
     	for(OrderRequest test:orderRequest)
     	{
     		orderProducer.sendOrder(test);
-    		System.err.println();
     	}
         return "Orders placed successfully!";
     }
